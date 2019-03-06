a0ba188d55433d3ab48595e84b42932b1f54a7b4
Irving Alain Aguilar Pérez <a01703171@itesm.mx>
2019-03-05 20:36:03 -0600

    Update LAB 11 to DAW_LABS

---
+++ DAW_LABS/LAB 11/index.php
@@ -0,0 +1,36 @@
+<?php
+    if(isset($_POST['submit'])){
+        $nombre = $_POST['nombre'];
+        $edad = $_POST['edad'];
+        $usuario = $_POST['usuario'];
+        $correo = $_POST['correo'];
+        $phone = $_POST['phone'];
+    }
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Registro de usuarios</title>
+        <link type="text/css" rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                <h1>Registro</h1>
+                <label for="">Nombre:</label>
+                <input type="text" name="nombre" placeholder="Primer nombre y apellido"  value="">
+                <label for="">Edad:</label>
+                <input type="text" name="edad" placeholder="Edad" value="">
+                <label for="">Usuario:</label>
+                <input type="text" name="usuario" placeholder="Usuario a 10 caracteres"  value="">
+                <label for="">Correo Electrónico:</label>
+                <input type="text" name="correo" placeholder="Correo" value="">
+                <label for="">Teléfono:</label>
+                <input type="text" name="phone" placeholder="Teléfono a 10 dígitos" value="">
+                <input type="submit" name="submit">
+                <?php
+                    include("validate-form.php");
+                ?>
+            </form>
+    </body>
+</html>
---
+++ DAW_LABS/LAB 11/result-view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Registro de usuarios</title>
+    </head>
+    <body>
+        <p>
+            Tu usuario <strong><?php echo $usuario ?></strong> ha quedado registrado a nombre de <strong><?php echo $nombre ?></strong> con el siguiente correo electrónico : <strong><?php echo $correo ?></strong> y telefono : <strong><?php echo $phone ?></strong>
+        </p>
+        <a href="index.php">Registra a otro usuario</a>
+    </body>
+</html>
